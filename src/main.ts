28b7b5635c3fc5ed7874e6863d2f36d3927cec09
LahmerMohammed <tonysycho14@gmail.com>
2021-07-22 17:09:34 +0100

    add swager

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs',app,document);
+
+  
   await app.listen(3000);
 }
 bootstrap();
