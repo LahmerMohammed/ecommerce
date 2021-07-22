13afc9127f76d440722243afa84560f1d3d0efb4
LahmerMohammed <tonysycho14@gmail.com>
2021-07-22 10:48:02 +0100

    add

---
+++ src/database/migration/1626945283184-updateAdress.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class updateAdress1626945283184 implements MigrationInterface {
+    name = 'updateAdress1626945283184'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_217ba147c5de6c107f2fa7fa271"`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "addressId" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_217ba147c5de6c107f2fa7fa271" FOREIGN KEY ("addressId") REFERENCES "address"("id") ON DELETE SET NULL ON UPDATE CASCADE`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_217ba147c5de6c107f2fa7fa271"`);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "addressId" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_217ba147c5de6c107f2fa7fa271" FOREIGN KEY ("addressId") REFERENCES "address"("id") ON DELETE SET NULL ON UPDATE CASCADE`);
+    }
+
+}
