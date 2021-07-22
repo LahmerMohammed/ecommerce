5be7493469f53591394bfbe4a2e8c06b785e036d
LahmerMohammed <tonysycho14@gmail.com>
2021-07-22 17:09:01 +0100

    add

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,4 +1,16 @@
+import { Crud } from '@nestjsx/crud';
+import { ReviewEntity } from './../../database/entities/review.entity';
+import { CrudController } from '@nestjsx/crud';
 import { Controller } from '@nestjs/common';
+import { ReviewService } from './review.service';
 
-@Controller('review')
-export class ReviewController {}
+@Crud({
+  model: {
+    type: ReviewEntity,
+  },
+})
+@Controller('reviews')
+export class ReviewController implements CrudController<ReviewEntity>{
+  constructor(public service : ReviewService){
+  }
+}
--- src/modules/review/review.service.ts
+++ src/modules/review/review.service.ts
@@ -1,4 +1,11 @@
+import { ReviewEntity } from './../../database/entities/review.entity';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService extends TypeOrmCrudService<ReviewEntity>{
+  constructor(@InjectRepository(ReviewEntity) repo){
+    super(repo);
+  }
+}
