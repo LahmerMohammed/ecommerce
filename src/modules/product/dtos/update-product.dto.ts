9dc0f2903d415992ad9c7956586f7421784f9fcd
LahmerMohammed <tonysycho14@gmail.com>
2021-07-29 18:24:04 +0100

    update

--- src/modules/product/dtos/update-product.dto.ts
+++ src/modules/product/dtos/update-product.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { Expose } from 'class-transformer';
+import { IsUUID } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends OmitType(CreateProductDto,["added_by_admin_id"]) {
+  @Expose()
+  @IsUUID()
+  updated_by_admin_id: string;
+}
