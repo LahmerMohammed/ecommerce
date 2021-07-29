3aee05af9bc92e7279214a3ac5f3dbfca0fa0d30
LahmerMohammed <tonysycho14@gmail.com>
2021-07-29 18:24:27 +0100

    fix

--- src/modules/product/dtos/update-product.dto.ts
+++ src/modules/product/dtos/update-product.dto.ts
@@ -3,7 +3,7 @@ import { Expose } from 'class-transformer';
 import { IsUUID } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends OmitType(CreateProductDto,["added_by_admin_id"]) {
+export class UpdateProductDto extends PartialType(OmitType(CreateProductDto,["added_by_admin_id"])) {
   @Expose()
   @IsUUID()
   updated_by_admin_id: string;
