1044fd4ed48b281c23445a3c93ee00189eec7de7
LahmerMohammed <tonysycho14@gmail.com>
2021-08-02 02:17:12 +0100

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { UserModule } from "./modules/user/user.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ReviewModule } from "./modules/review/review.module";
 import { AuthModule } from "./modules/auth/auth.module";
-import typeOrmConfig from "./database/config";
+
+import typeOrmConfig = require("./database/config");
 
 @Module({
   imports: [
