57bc82b8ed0fff72097c60db5260261ab37ab5d9
LahmerMohammed <tonysycho14@gmail.com>
2021-08-13 21:19:39 +0100

    update

---
+++ src/guards/product-owner.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "src/database/entities/role.enum";
+import { ROLES_KEY } from "src/decorators/role.decorator";
+import { ProductService } from "src/modules/product/product.service";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector,
+    private readonly productService:  ProductService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    
+
+    const { user } = context.switchToHttp().getRequest();
+    const { body } = context.switchToHttp().getRequest();
+    
+    const isOwner = await this.productService.isOwner(body.user_id,body.product_id);
+
+    return requiredRoles.some(role => user.roles.includes(role) ) 
+
+  }
+}
