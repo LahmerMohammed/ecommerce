b20e6f0e0b3395227c6de9c10878ed0283c7ca9f
LahmerMohammed <tonysycho14@gmail.com>
2021-08-13 20:55:10 +0100

    add role guard

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,25 +1,25 @@
-import { ROLES_KEY } from './../decorators/role.decorator';
-import { Role } from './../database/entities/role.enum';
-
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, NotImplementedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-
-
-// I don't nothing about this !
+import { Role } from 'src/database/entities/role.enum';
+import { ROLES_KEY } from 'src/decorators/role.decorator';
+import { ProductService } from 'src/modules/product/product.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector,
+    private readonly productService:  ProductService) {}
 
   canActivate(context: ExecutionContext): boolean {
+    
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (!requiredRoles) {
-      return true;
-    }
+    
+
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+
+    return requiredRoles.some(role => user.roles.includes(role) );
+
   }
 }
