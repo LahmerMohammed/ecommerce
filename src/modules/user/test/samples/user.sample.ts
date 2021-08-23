5c1794ff0017119c8407d14433364658831389e8
LahmerMohammed <tonysycho14@gmail.com>
2021-08-23 16:06:06 +0100

    add

--- src/modules/user/test/samples/user.sample.ts
+++ src/modules/user/test/samples/user.sample.ts
@@ -1,9 +1,38 @@
-import { AddProductWhishlist } from "../../dtos/whsihlist-dtos/add-product-whsilst.dto"
+import { ProductEntity } from './../../../../database/entities/product.entity';
+import { UserEntity } from './../../../../database/entities/user.entity';
+import { UpdateUserWhishlistDto, ACTION } from './../../dtos/update-user-whishlist.dto';
 
 
-export const getProductWhishlistSample = () : AddProductWhishlist => {
+/**
+ * 
+ * @args for 'userService.updateUserWhishlist' function
+ */
+export const getProductWhishlistSample = (action: ACTION) : UpdateUserWhishlistDto => {
     return {
       product_id: '1',
       user_id: '1',
+      action: action,
     }
 }
+
+export const getProductStub = () : ProductEntity => {
+  const product = new ProductEntity();
+
+  product.id = '1';
+
+  return product;
+}
+
+// return of 'userService.updateUserWhishlist' function
+export const getUserWhishlistStub = () : UserEntity => {
+  const user = new UserEntity();
+  const product = new ProductEntity();
+  
+  product.id='1';
+  user.id = '1';
+  user.whishlist = [product];
+
+  return user;
+
+}
+
