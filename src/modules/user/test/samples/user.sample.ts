2bb65714db2ba263ecb32f5f87c8332f8877032f
LahmerMohammed <tonysycho14@gmail.com>
2021-08-23 09:21:00 +0100

    add

---
+++ src/modules/user/test/samples/user.sample.ts
@@ -0,0 +1,9 @@
+import { AddProductWhishlist } from "../../dtos/whsihlist-dtos/add-product-whsilst.dto"
+
+
+export const getProductWhishlistSample = () : AddProductWhishlist => {
+    return {
+      product_id: '1',
+      user_id: '1',
+    }
+}
