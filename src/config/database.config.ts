df870d58291edb205eed21bf79944af93bdacb8b
LahmerMohammed <tonysycho14@gmail.com>
2021-09-09 11:48:28 +0100

    fix ssl v3

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -20,6 +20,7 @@ function getOptions() {
       connectionOptions,
       {
         url: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
         extra: {
           ssl: true
         },
