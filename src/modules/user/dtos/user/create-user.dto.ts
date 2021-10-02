a8c7749e96b69dfb0ff1c27eaac020611c74087f
LahmerMohammed <tonysycho14@gmail.com>
2021-10-02 11:42:02 +0100

    update

--- src/modules/user/dtos/user/create-user.dto.ts
+++ src/modules/user/dtos/user/create-user.dto.ts
@@ -2,29 +2,6 @@ import { Role } from "src/database/entities/role.enum";
 import { IsAlpha, IsAlphanumeric, IsBIC, IsISO31661Alpha2, IsDataURI, IsDate, IsEmail, IsEnum, IsLowercase, IsObject, IsPostalCode, IsString, Length, Min, ValidateNested, IsDateString, IsArray, isPhoneNumber, IsPhoneNumber } from 'class-validator';
 import { Exclude, Expose } from "class-transformer";
 
-//make custom validator for address
-
-@Expose()
-class AddressDto {
-
-  @Exclude()
-  id: string;
-
-  @IsString()
-  street: string;
-
-  @IsISO31661Alpha2()
-  country: string;
-  
-  @IsString()
-  city: string;
-  
-  @IsString()
-  town: string;
-
-  @IsPostalCode()
-  zipcode: string;
-}
 
 @Exclude()
 export class CreateUserDto {
