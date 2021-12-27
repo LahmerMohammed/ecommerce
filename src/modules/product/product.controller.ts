40bda1617112398d00be383a1a542940716a3f01
Lahmer Mohammed <tonysycho14@gmail.com>
2021-12-27 14:56:17 +0100

    fix: dto validation

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,10 +1,11 @@
+import { plainToClass } from 'class-transformer';
 import { UserEntity } from 'src/database/entities/user.entity';
 import { JwtAuthGuard } from './../auth/guards/jwt.guard';
 import { RolesGuard } from './../../guards/role.guard';
 import { UpdateProductDto } from './dtos/update-product.dto';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { Crud, CrudController, Override } from '@nestjsx/crud';
-import { Controller, Post, UseGuards, Body, UseInterceptors, UploadedFiles, UploadedFile, Req, Delete, Put } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, UseInterceptors, UploadedFiles, UploadedFile, Req, Delete, Put, BadRequestException } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductEntity } from 'src/database/entities/product.entity';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
@@ -14,6 +15,8 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { request } from 'http';
 import { DeleteImageDto } from './dtos/delete-image.dto';
 import { User } from 'src/decorators/user.decorator';
+import { isUUID, IsUUID, validate, validateOrReject, ValidationError, Length } from 'class-validator';
+import { BadyValidation } from 'src/decorators/body-validation.decorator';
 
 
 @ApiTags('products')
@@ -55,21 +58,23 @@ export class ProductController implements CrudController<ProductEntity> {
         },
         createProductDto:{
           type: 'object',
+          
         }
       },
     },
   }) 
   @UseInterceptors(FilesInterceptor('images'))
-  /**
+  /** 
    * TODO : set 'createProductDto' to 'CreateProductDto' or add data validation manually 
    */
   async addProduct(@UploadedFiles() images: Array<Express.Multer.File>,
-                   @Body("product_dto") createProductDto: any)
+                   @BadyValidation(CreateProductDto) createProductDto: CreateProductDto)
   {
+
     images.forEach(file => {
       console.log(file.originalname) 
     });
-    console.log(createProductDto);
+    console.log(createProductDto); 
     
     return this.service.createOneBase(createProductDto , images);
   }
@@ -104,11 +109,14 @@ export class ProductController implements CrudController<ProductEntity> {
   @UseInterceptors(FilesInterceptor('images'))
   @Put('/images')
   async addImage(@UploadedFiles() images: Array<Express.Multer.File>,
-                 @User("id") user_id: string,
+                 @User("id")  user_id: string,
                  @Body("product_id") product_id: string)
   {
+   if( !isUUID(product_id) ){
+     throw new BadRequestException('product_id must be valid uuid');
+   }
 
-   const body = {product_id , images}
+   const body = { product_id , images};
    return await this.service.addImages(user_id , body ); 
   }
 }
