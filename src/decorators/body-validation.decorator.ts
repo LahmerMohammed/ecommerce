254a8887fe17239b7ca2c4c97c4c54abf4dc490c
Lahmer Mohammed <tonysycho14@gmail.com>
2021-12-27 14:55:59 +0100

    create

---
+++ src/decorators/body-validation.decorator.ts
@@ -0,0 +1,42 @@
+import {  validate, validateOrReject } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+
+
+export const BadyValidation = createParamDecorator(
+  async (Dto: any , ctx: ExecutionContext) => {
+    
+    const body = ctx.switchToHttp().getRequest().body;
+
+
+    const dto = plainToClass(Dto , body);
+   
+  
+    /* validate(dto).then(errors => {
+      
+      }) */
+
+    try{
+      await validateOrReject(dto);
+
+    }catch(errors){
+      
+      var constraints = []
+      
+      if( errors.length > 0){
+
+        errors.forEach(error => {
+ 
+         for(var key in error.constraints){
+           constraints.push(error.constraints[key])
+        }
+      });  
+     }
+     throw new BadRequestException(constraints)
+    }
+
+    return dto;
+
+  }
+)
