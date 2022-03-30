dd72093a666d651f5a04c4bdbb66cec9c4ca771e
LahmerMohammed <lahmermohammed65@gmail.com>
2022-03-30 23:40:17 +0100

    fix: [order table already exist]

--- src/database/migration/1640129464038-updateWhishList.ts
+++ src/database/migration/1640129464038-updateWhishList.ts
@@ -4,7 +4,6 @@ export class updateWhishList1640129464038 implements MigrationInterface {
     name = 'updateWhishList1640129464038'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "order" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "status" character varying NOT NULL, "purshase_date" TIMESTAMP NOT NULL, "subtotal_price" integer NOT NULL, "shipping_fee" integer NOT NULL, "discount" integer NOT NULL, CONSTRAINT "PK_1031171c13130102495201e3e20" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "price"`);
         await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "quantity"`);
         await queryRunner.query(`ALTER TABLE "product" ADD "sale_price" integer NOT NULL`);
@@ -20,7 +19,6 @@ export class updateWhishList1640129464038 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "product" DROP COLUMN "sale_price"`);
         await queryRunner.query(`ALTER TABLE "product" ADD "quantity" integer NOT NULL`);
         await queryRunner.query(`ALTER TABLE "product" ADD "price" integer NOT NULL`);
-        await queryRunner.query(`DROP TABLE "order"`);
     }
 
 }
